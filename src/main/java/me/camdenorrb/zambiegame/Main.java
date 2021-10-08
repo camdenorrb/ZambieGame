0bce1d8c49c88a42221afde20d311951fc5c26d2
camdenorrb <5360339+camdenorrb@users.noreply.github.com>
2021-10-08 12:48:30 -0500

    Clean up comments in Main function

--- src/main/java/me/camdenorrb/zambiegame/Main.java
+++ src/main/java/me/camdenorrb/zambiegame/Main.java
@@ -5,33 +5,32 @@ package me.camdenorrb.zambiegame;
  */
 public class Main {
 
-	//private static final MiniBus MINI_BUS = new MiniBus();
-
-
 	public static void main(String[] args) {
 
 		new TitleScreen().start();
 
 
-		//final SwingGui gui = new SwingGui("Meow", new Dimension(1000, 1000));
+		/*
+			final SwingGui gui = new SwingGui("Meow", new Dimension(1000, 1000));
 
-		//final ZambieGame game = new ZambieGame();
-		//game.start();
+			final ZambieGame game = new ZambieGame();
+			game.start();
 
-		//new ZambieGame().start();
+			new ZambieGame().start();
 
-		//Thread.sleep(1000);
+			Thread.sleep(1000);
 
-		/*final ProcGui gui = new ProcGui("Meow", new Dimension(1000, 500));
-		gui.show();
+			final ProcGui gui = new ProcGui("Meow", new Dimension(1000, 500));
+			gui.show();
 
-		Thread.sleep(1000);
-		gui.hide();
+			Thread.sleep(1000);
+			gui.hide();
 
-		Thread.sleep(1000);
-		gui.show();*/
+			Thread.sleep(1000);
+			gui.show();
 
-		//new SwingGui(new Dimension(1000, 1000)).show();
+			new SwingGui(new Dimension(1000, 1000)).show();
+		*/
 	}
 
 }
