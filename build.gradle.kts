fada5aa65ee6cff05b8c1daed55086cdc3d37ecc
camdenorrb <me@12oclock.dev>
2026-02-14 03:06:57 -0600

    Fix shadowjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     idea
     application
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.3.1"
     //kotlin("jvm") version "1.3.31"
 }
 
